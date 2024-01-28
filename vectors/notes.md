77ad79f073930064b5fbd30c5ad0f29c921a65f0
milankocza <kocza.milan@gmail.com>
2024-01-28 12:55:31 +0100

    Bouncing ball with vectors

---
+++ vectors/notes.md
@@ -0,0 +1,8 @@
+#Force
+A force is a vector that causes an object with mass to accelerate.
+
+#Vector
+Vector is an entity with magnitude and direction
+
+#Acceleration
+change of velocity during time
---
+++ vectors/sketch.js
@@ -0,0 +1,26 @@
+let position;
+let velocity;
+
+function setup() {
+    createCanvas(640, 240);
+
+    position = createVector(100, 100);
+    velocity = createVector(1, 3.33);
+}
+
+function draw() {
+    background(255);
+
+    position.add(velocity);
+
+    if (position.x > width || position.x < 0) {
+        velocity.x = velocity.x * -1;
+    }
+    if (position.y > height || position.y < 0) {
+        velocity.y = velocity.y * -1;
+    }
+
+    stroke(0);
+    fill(127);
+    circle(position.x, position.y, 48);
+}
